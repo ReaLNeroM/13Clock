e9a1b30cf1981ed1e42f6de2af28aa1e4313998c
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-09-17 13:28:29 -0400

    Initial commit

--- drawing.js
+++ drawing.js
@@ -1,85 +1,74 @@
-var canvas = document.getElementById('canvas');
+ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-console.log(window.innerWidth);
-var radius = Math.min(window.innerWidth, window.innerHeight) / 2;
-canvas.width = canvas.height = 2 * radius;
-
+var radius = Math.min(window.innerWidth, window.innerHeight);
+canvas.width = canvas.height = radius;
 var fullRotation = 2 * Math.PI;
-ctx.translate(radius, radius);
 
-var minutesPerTurn = 43200000 / 13.0; // 12 + 1
-var secondsPerTurn = 43200000 / 733.0; // 12 * (60 + 1) + 1
+var circleSize = canvas.width / 20;
+console.log(canvas.width);
 
-var FPS = 60;
-setInterval(draw, 1000.0 / FPS);
-
-
-function getMsSinceMidnight(d) {
-	var start = new Date();
-	start = new Date(start.getFullYear(),
-						start.getMonth(),
-						start.getDate() ,
-						0, 0, 0);
-	
-	return d - start;
+function getNthCircleX(circleNumber){
+	return circleNumber / 10 * (canvas.width + (canvas.width - 2 * circleSize) / 9 * 10 - canvas.width) + circleSize;
 }
 
-function draw(){
-	ctx.fillStyle = '#fff';
-	ctx.fillRect(-radius, -radius, canvas.width, canvas.height);
-
-	currTime = new Date();
-	var milliseconds = getMsSinceMidnight(currTime) % 43200000; // 12 hour clock
-
-	drawCircle(radius, 2);
-	drawNotches(radius, 1.5, 20);
-	drawHand(6, radius / 5, milliseconds / 43200000);
-	drawHand(4, radius / 1.4, milliseconds % minutesPerTurn / minutesPerTurn);
-	ctx.strokeStyle = "#FF0000";
-	drawHand(3, radius / 1.1, milliseconds % secondsPerTurn / secondsPerTurn);
-	ctx.strokeStyle = "#000000";
-}
-
-function drawCircle(radius, width){
-	ctx.lineWidth = width;
-	ctx.lineCap = "round";
-
-	ctx.beginPath();
-	
-	ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-	ctx.stroke();
-}
-
-function drawNotches(radius, width, notchLength){
-	ctx.lineWidth = width;
-	ctx.lineCap = "butt";
-
-	for (var i = 0; i < 12; i++){
+function drawCircles(rBitset, gBitset, bBitset){
+	ctx.fillStyle = "#FFF";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.strokeStyle = "#000";
+	for(var i = 0, pow2 = 1; i < 10; i++, pow2 *= 2){
+		color = "#";
+		if(pow2 & rBitset){
+			color += "F";
+		} else {
+			color += "0";
+		}
+		if(pow2 & gBitset){
+			color += "F";
+		} else {
+			color += "0";
+		}
+		if(pow2 & bBitset){
+			color += "F";
+		} else {
+			color += "0";
+		}
+		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.moveTo(0, 0);
-
-		ctx.rotate( i / 12 * fullRotation);
-
-		ctx.moveTo(0, (radius - notchLength));
-		ctx.lineTo(0, radius);
+		ctx.arc(getNthCircleX(i), canvas.height / 2, circleSize, 0, fullRotation);
+		ctx.fill();
 		ctx.stroke();
-
-		ctx.rotate(-i / 12 * fullRotation);
+		ctx.closePath();
 	}
 }
 
-function drawHand(width, length, rotation){
-	ctx.lineWidth = width;
-	ctx.lineCap = "round";
-
+function drawCurve(start, end, extent, color){
 	ctx.beginPath();
-	ctx.moveTo(0, 0);
+	ctx.strokeStyle = color;
 
-	ctx.rotate(rotation * fullRotation);
-	
-	ctx.lineTo(0, -length);
+	var startX = getNthCircleX(start);
+	var endX = getNthCircleX(end);
+
+	ctx.moveTo(startX, canvas.height / 2);
+	ctx.bezierCurveTo(startX, canvas.height / 2 + extent, endX, canvas.height / 2 + extent, endX, canvas.height / 2);
 	ctx.stroke();
-
-	ctx.rotate(-rotation * fullRotation);
+	ctx.closePath();
+	ctx.strokeStyle = "#FFF";
 }
+
+function drawTime(){
+	var start = new Date();
+	var hour, minute, second;
+	hour = start.getHours();
+	minute = start.getMinutes();
+	second = start.getSeconds();
+
+	drawCircles((1 << Math.floor(hour / 10  )) | (1 << Math.floor(hour % 10  )),
+				(1 << Math.floor(minute / 10)) | (1 << Math.floor(minute % 10)),
+				(1 << Math.floor(second / 10)) | (1 << Math.floor(second % 10)));
+	drawCurve(Math.floor(hour / 10), Math.floor(hour % 10), 100.0, "#F00");
+	drawCurve(Math.floor(minute / 10), Math.floor(minute % 10), 300.0, "#0F0");
+	drawCurve(Math.floor(second / 10), Math.floor(second % 10), 320.0, "#00F");
+}
+
+setInterval(drawTime, 500);
